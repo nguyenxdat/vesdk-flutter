b0123beb2b402242bf213ab8acafbf9bf4928a42
thucdx <thucdx1@nal.vn>
2022-09-08 08:16:08 +0700

    change text, icon ios

--- ios/Classes/FlutterVESDK.swift
+++ ios/Classes/FlutterVESDK.swift
@@ -4,6 +4,23 @@ import ImglyKit
 import imgly_sdk
 import AVFoundation
 
+private extension UIImage {
+  /// Create a new icon image for a specific size by centering the input image and optionally applying alpha blending.
+  /// - Parameters:
+  ///   - pt: Icon size in point (pt).
+  ///   - alpha: Icon alpha value.
+  /// - Returns: A new icon image.
+  func icon(pt: CGFloat, alpha: CGFloat = 1, backgroundColor: UIColor = UIColor.red) -> UIImage? {
+    UIGraphicsBeginImageContextWithOptions(CGSize(width: pt, height: pt), false, scale)
+    let position = CGPoint(x: (pt - size.width) / 2, y: (pt - size.height) / 2)
+    draw(at: position, blendMode: .normal, alpha: alpha)
+    backgroundColor.setFill()
+    let newImage = UIGraphicsGetImageFromCurrentImageContext()
+    UIGraphicsEndImageContext()
+    return newImage
+  }
+}
+
 @available(iOS 13.0, *)
 public class FlutterVESDK: FlutterIMGLY, FlutterPlugin, VideoEditViewControllerDelegate {
 
@@ -109,6 +126,43 @@ public class FlutterVESDK: FlutterIMGLY, FlutterPlugin, VideoEditViewControllerD
 
             var photoEditModel = PhotoEditModel()
             var videoEditViewController: VideoEditViewController
+
+            let config = UIImage.SymbolConfiguration(scale: .large)
+                // highlight-config
+                // Set up the image replacement closure (once) before the editor is initialized.
+                // highlight-bundle-image
+            IMGLY.bundleImageBlock = { imageName in
+              // highlight-bundle-image
+              // Print the image names that the SDK is requesting at run time. This allows to interact with the editor and
+              // to identify the image names of icons that should be replaced. Alternatively, all default assets can be found
+              // in the `ImglyKit.bundle` located within the `ImglyKit.framework`s, e.g., in the directory:
+              // `ImglyKit.xcframework/ios-arm64/ImglyKit.framework/ImglyKit.bundle/`
+              // Return replacement images for the requested image name.
+              // Most icon image names use the `pt` postfix which states the expected dimensions for the used image measured
+              // in points (pt), e.g., the postfix `_48pt` stands for an image of 48x48 pixels for scale factor 1.0 and 96x96
+              // pixels (@2x) as well as 144x144 pixels (@3x) for its high-resolution variants.
+              // highlight-switch
+              switch imageName {
+              // toolbar appearance.
+              case "imgly_icon_save":
+                return UIImage(systemName: "checkmark.circle.fill", withConfiguration: config)?.icon(pt: 44, alpha: 0.6, backgroundColor: UIColor.red)
+              // Returning `nil` will use the default icon image.
+              default:
+                return nil
+              }
+              // highlight-switch
+            }
+
+            VESDK.localizationBlock = { stringToLocalize in
+                  switch stringToLocalize {
+                  case "pesdk_editor_text_exportProgressUnknown":
+                    return "Saving..."
+                  case "pesdk_editor_text_exportProgress":
+                    return "Saving video"
+                  default:
+                    return nil
+                  }
+                }
 
             if let _serialization = serializationData {
                 let deserializationResult = Deserializer.deserialize(data: _serialization, imageDimensions: video.size, assetCatalog: configurationData?.assetCatalog ?? .defaultItems)
